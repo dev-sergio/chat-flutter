84e566e2ed270abf3e442b92c9ff3413c76edc8f
Sergio Junior <sergio.ltnj@gmail.com>
2022-03-31 12:31:01 -0300

    Fazendo login com google signin

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,12 +1,13 @@
 import 'dart:io';
 
 import 'package:chat/text_composer.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
-
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -16,13 +17,50 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-
   FirebaseFirestore firestore = FirebaseFirestore.instance;
   List<Map<dynamic, dynamic>> list = [];
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  User? _currentUser;
 
+  Future<User?> _getUser() async {
+    if (_currentUser != null) return _currentUser;
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      final UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+
+      final User? user = userCredential.user;
+      print('Usuario: $user');
+      return user;
+    } catch (error) {
+      print(error);
+    }
+  }
 
   void _sendMessage({String? text, XFile? imgFile}) async {
-    Map<String, dynamic> data = {};
+    final User? user = await _getUser();
+
+    if (user == null) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text("Não foi possivel fazer o login, tente novamente!"),
+        backgroundColor: Colors.red,
+      ));
+    }
+
+    Map<String, dynamic> data = {
+      "uid": user?.uid,
+      "senderName": user?.displayName,
+      "senderPhoto": user?.photoURL
+
+    };
 
     if (imgFile != null) {
       File file = File(imgFile.path);
@@ -36,7 +74,6 @@ class _ChatScreenState extends State<ChatScreen> {
         String url = await (snapshot).ref.getDownloadURL();
         data['imgUrl'] = url;
       } on FirebaseException catch (e) {
-
         if (kDebugMode) {
           print(task.snapshot);
         }
@@ -62,33 +99,41 @@ class _ChatScreenState extends State<ChatScreen> {
       body: Column(
         children: [
           Expanded(
-              child: StreamBuilder<QuerySnapshot>(
-                stream: FirebaseFirestore.instance.collection('messages').snapshots(),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData ){
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    ) ;
-                  }
-
-                  List<DocumentSnapshot> lista = snapshot.data!.docs.reversed.toList();
-                  return ListView.builder(
-                    itemCount: snapshot.data!.docs.length,
-                    reverse: true,
-                    itemBuilder: (context, index){
-                      return ListTile(
-                        title: Text(lista[index]['text']),
-                      );
-                    },
-
+            child: StreamBuilder<QuerySnapshot>(
+              stream:
+                  FirebaseFirestore.instance.collection('messages').snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
                   );
-                },
+                }
 
-              ),
+                List<DocumentSnapshot> lista =
+                    snapshot.data!.docs.reversed.toList();
+                return ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  reverse: true,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(lista[index]['text']),
+                    );
+                  },
+                );
+              },
+            ),
           ),
           TextComposser(sendMessage: _sendMessage),
         ],
       ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.authStateChanges().listen((user) {
+      _currentUser = user;
+    });
   }
 }
